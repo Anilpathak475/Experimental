d4e0a4aba05ab770b48c15b474976739e34e3531
anilpathak <anilpathak475@gmail.com>
2018-05-30 13:07:34 +0530

    dropdown for country custom adapter for spinner created

---
+++ app/src/main/java/com/bitnudge/ime/demo/modle/Country.java
@@ -0,0 +1,20 @@
+package com.bitnudge.ime.demo.modle;
+
+public class Country {
+
+    private String name;
+    private int id;
+
+    public Country(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
